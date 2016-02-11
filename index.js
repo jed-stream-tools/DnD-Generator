1558dc9bc4305870edbc4108f5b9082e2dc3d3b6
Jordan Ide <jorrdandied@hotmail.com>
2016-02-11 17:25:49 +0000

    Fixed "undefined" outputs

--- index.js
+++ index.js
@@ -14,12 +14,13 @@ app.get('/', function(req,res) {
     res.send(generate());
 });
 
+
 function generate() {
-    let a = _adjectives[randomIntFromInterval(0, _adjectives.length)];
-    let r = _race[randomIntFromInterval(0, _race.length)];
-    let c = _class[randomIntFromInterval(0, _class.length)];
-    let l = _location[randomIntFromInterval(0, _location.length)];
-    let b = _backstory[randomIntFromInterval(0, _backstory.length)];
+    let a = _adjectives[randomIntFromInterval(0, _adjectives.length -1)];
+    let r = _race[randomIntFromInterval(0, _race.length -1)];
+    let c = _class[randomIntFromInterval(0, _class.length -1)];
+    let l = _location[randomIntFromInterval(0, _location.length -1)];
+    let b = _backstory[randomIntFromInterval(0, _backstory.length -1)];
 
     return `${a} ${r} ${c} from ${l} who ${b}`;
 }
